5274d33f95c356e73ad8478fb64c03783b20b4dc
prithvisaran3 <prithvisaran.s@gmail.com>
2024-10-08 20:18:36 -0400

    Initial format done (partial MVC)

---
+++ pawfect_ASP/Utils/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  pawfect_ASP
+//
+//  Created by Prithvi’s Macbook on 10/8/24.
+//
+
+import Foundation
+import SwiftUI
+
+var screenHeight: CGFloat{
+    UIScreen.main.bounds.height
+}
+var screenWidth: CGFloat{
+    UIScreen.main.bounds.width
+}
